de811e708031cd6557ff85940a70a7579570202c
qsceszwdvrdx <55731801+qsceszwdvrdx@users.noreply.github.com>
2019-10-11 19:58:45 +0800

    Create Modulus.py

---
+++ codesignal/python/Modulus.py
@@ -0,0 +1,14 @@
+  # 檢驗 n 是不是整數
+  
+  def modulus(n):
+      if isinstance(n, int):
+          return n % 2
+      else:
+          return -1
+
+
+  n%1==0
+  n==int(n)
+  n == n//1
+  type(n) == int
+  isinstance(n, int) 
