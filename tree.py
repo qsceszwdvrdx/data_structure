2a3691cc86c42d5d5e409e728a40ce628954cbc0
qsceszwdvrdx <55731801+qsceszwdvrdx@users.noreply.github.com>
2019-11-01 16:50:08 +0800

    Create tree.py

---
+++ tree.py
@@ -0,0 +1,23 @@
+class linkedlist()
+
+  self.val = None
+  self.next = None
+
+
+class node:
+
+  def __int__(self,val):
+   
+   self.root=None
+   self.size=0
+   self.element=element
+   self.parent=parent
+   self.left=left
+   self.right=right
+  
+  def left(self):
+  
+  
+  def right(self):
+  
+  ....
